736b099626d58eaef8e7c2d97a4c59921b66369b
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-05-15 00:25:45 +0300

    функция заменена на метод

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProject.java
@@ -1,27 +1,24 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProject {
-	public static void main(String[] args) {
-		hello ("Any");
-		hello ("Maria");
+  public static void main(String[] args) {
+    hello("Any");
+    hello("Maria");
 
-		Square s = new Square(7);
-		//s.l = 8;
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    Square s = new Square(7);
+    //s.l = 8;
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		Rectangle r = new Rectangle(5, 6);
+    Rectangle r = new Rectangle(5, 6);
     //r.a = 8;
-		//r.b = 3;
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
-	}
-	public static void hello (String somebody){
-		System.out.println("Hello, " + somebody + "!");
+    //r.b = 3;
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-	}
-	public static double area (Square s){
-		return s.l * s.l;
-	}
-	public static double area(Rectangle r){
-		return r.a * r.b;
-	}
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
 }
