7d9d6f3e90d370e7b9f025c8814dc53e7e74b651
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-05-21 22:32:41 +0300

    добавлен класс ContactModificateTests, обновлен класс-помощник "GroupsHelper"

---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupsHelper().selectGroup();
+    app.getGroupsHelper().initGroupModification();
+    app.getGroupsHelper().fillGroupForm(new GroupData("name1", "header1", "footer1"));
+    app.getGroupsHelper().modificateSelectedGroup();
+    app.getGroupsHelper().returnToGroupPage();
+  }
+}
