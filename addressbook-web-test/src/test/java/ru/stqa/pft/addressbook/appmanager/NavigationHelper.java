98521884e450b69134e58199321603caabd84bfb
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-05-28 14:26:58 +0300

    if else

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.firefox.FirefoxDriver;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
