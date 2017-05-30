e757c91433670dd7737efb3bff4b73e23aa7327a
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-05-30 17:07:37 +0300

    upd ContactdeletionTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,19 +5,16 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class ContactDeletionTests extends TestBase{
-  @Test
-  public void testContactDeletion() {
-    if (!app.getGroupsHelper().isThereAGroup()){
-      app.getGroupsHelper().createGroup(new GroupData("name", null, null));
+public class ContactDeletionTests extends TestBase {
+    @Test
+    public void testContactDeletion() {
+        app.getNavigationHelper().goToContactsList();
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().createContact(new ContactData("firstName", "middleName", "lastName", "address", "home", "mobile", "email", "name"));
+        }
+        app.getContactHelper().selectContact();
+        app.getContactHelper().deleteSelectedContact();
+        app.getContactHelper().switchAlert();
+        app.getNavigationHelper().goToContactsList();
     }
-    app.getNavigationHelper().goToContactsList();
-    if (!app.getContactHelper().isThereAContact()){
-      app.getContactHelper().createContact(new ContactData("firstName", "middleName", "lastName", "address", "home", "mobile", "email", "name"));
-    }
-    app.getContactHelper().selectContact();
-    app.getContactHelper().deleteSelectedContact();
-    app.getContactHelper().switchAlert();
-    app.getNavigationHelper().goToContactsList();
-  }
 }
