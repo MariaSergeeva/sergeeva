0e4c767e5db33040a9bdb632f211d6acad6b96c1
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-06-13 16:18:02 +0300

    сравнение множеств групп для теста модификации группы

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+  private final String groupId;
   private final String groupName;
   private final String groupHeader;
   private final String groupFooter;
 
   public GroupData(String groupName, String groupHeader, String groupFooter) {
+    this.groupId = null;
+    this.groupName = groupName;
+    this.groupHeader = groupHeader;
+    this.groupFooter = groupFooter;
+  }
+  public GroupData(String groupId1, String groupName, String groupHeader, String groupFooter) {
+    this.groupId = groupId1;
     this.groupName = groupName;
     this.groupHeader = groupHeader;
     this.groupFooter = groupFooter;
@@ -22,12 +30,8 @@ public class GroupData {
   public String getGroupFooter() {
     return groupFooter;
   }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "groupName='" + groupName + '\'' +
-            '}';
+  public String getGroupId() {
+    return groupId;
   }
 
   @Override
@@ -37,11 +41,23 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (groupId != null ? !groupId.equals(groupData.groupId) : groupData.groupId != null) return false;
     return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
   }
 
   @Override
   public int hashCode() {
-    return groupName != null ? groupName.hashCode() : 0;
+    int result = groupId != null ? groupId.hashCode() : 0;
+    result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
+    return result;
   }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "groupId='" + groupId + '\'' +
+            ", groupName='" + groupName + '\'' +
+            '}';
+  }
+
 }
