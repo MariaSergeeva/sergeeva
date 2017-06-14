92da0d23d36ca56f43e0c8921b6ce157d9b5fdd9
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-06-14 10:49:32 +0300

    анонимная функция для поиска макс.id в тесте создания группы

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -19,13 +18,7 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupsHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getGroupId() > max) {
-                max = g.getGroupId();
-            }
-        }
-        group.setGroupId(max);
+        group.setGroupId(after.stream().max((o1, o2) -> Integer.compare(o1.getGroupId(), o2.getGroupId())).get().getGroupId());
         before.add(group);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
