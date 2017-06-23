0c5a6a52359fe56eb13dbf82ce33736bcc420985
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-06-23 10:00:02 +0300

    update ContactDeletionTests

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -15,19 +15,19 @@ public class ContactDeletionTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().ContactsList();
-    if (app.contact().all().size() == 0) {
+    if (app.db().contacts().size() == 0) {
       app.contact().create(new ContactData().withFirstName(RandomStringUtils.randomAlphabetic(10)).withLastName(RandomStringUtils.randomAlphabetic(10)).withGroup(app.contact().groupName()));
     }
   }
 
   @Test
   public void testContactDeletion() {
-    Contacts before = app.contact().all();
+    Contacts before = app.db().contacts();
     ContactData deletedContact = before.iterator().next();
     app.contact().deleteById(deletedContact);
     app.goTo().ContactsList();
     assertEquals(app.contact().count(), (before.size() - 1));
-    Contacts after = app.contact().all();
+    Contacts after = app.db().contacts();
     MatcherAssert.assertThat(after, CoreMatchers.equalTo(before.without(deletedContact)));
   }
 }
