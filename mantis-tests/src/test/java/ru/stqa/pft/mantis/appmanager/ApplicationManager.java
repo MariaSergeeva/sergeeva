e979ba6db3f9134063df21ee8d779d10e49484c0
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-07-03 18:02:37 +0300

    LoginTests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -43,5 +43,13 @@ public class ApplicationManager {
   public void stop() {
     wd.quit();
   }
+
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
 
