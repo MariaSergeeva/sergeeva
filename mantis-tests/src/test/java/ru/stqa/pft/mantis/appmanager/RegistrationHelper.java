4bf5d6dcf034a9d47828a9eaf476a12b43b2e2cf
m.sergeeva@t1-group.ru <m.sergeeva@t1-group.ru>
2017-07-04 13:23:26 +0300

    Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,33 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.appmanager.HelperBase;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+import java.io.UnsupportedEncodingException;
 
-  public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+public class RegistrationHelper extends HelperBase {
+
+
+  public RegistrationHelper(WebDriver wd) {
+    super((ApplicationManager) wd);
   }
 
-  public void start(String username, String email) {
+  public RegistrationHelper(ApplicationManager app) {
+    super(app);
+  }
+
+  public void start(String username, String email) throws UnsupportedEncodingException {
     wd.get(app.getProperty("web.baseURL") + "/signup_page.php");
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.xpath(".//input[@value = 'Зарегистрироваться']"));
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.xpath(".//button[@type='submit']"));
   }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -7,3 +7,5 @@ $g_db_password            = '';
 $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'I8VsqoiPpWTgafQc2nTLfWLJhCOhLtZNfwSClr591jg=';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
